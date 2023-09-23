b952e689b5ed5ef2e685a232cb7d1604ba9d1952
lbischofbeger <141656977+lbischofbeger@users.noreply.github.com>
2023-09-23 21:44:24 +0200

    funktionirendes js script

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-inks = document.querySelectorAll('nav a');
+const sectionLinks = document.querySelectorAll('nav a');
 const sections = document.querySelectorAll('.hidden-section');
 const homeButton = document.getElementById('home-button');
 const contentSection = document.getElementById('content');
 
-orEach(link => {
+sectionLinks.forEach(link => {
     link.addEventListener('click', function(event) {
-        event.preventDefault();
+        event.preventDefault(); // Verhindert das Navigieren zur Seite
+
+        // Holen Sie sich den Zielabschnitt und seine Position
         const targetSection = document.getElementById(link.getAttribute('data-section'));
+
+        // Verstecken Sie alle Abschnitte
         sections.forEach(section => {
             section.classList.add('hidden-section');
         });
+
+        // Fügen Sie den Zielabschnitt direkt unter den ausgewählten Link ein und zeigen Sie ihn an
         link.parentElement.insertBefore(targetSection, link.nextElementSibling);
         targetSection.classList.remove('hidden-section');
     });
@@ -22,17 +28,16 @@ homeButton.addEventListener('click', function() {
 });
 
 
-
-
-
 document.getElementById("info-button").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "block";
+    popup.style.display = "block";
+    sections.forEach(section => {
+        section.classList.add('hidden-section');
+    });
 });
 
-document.getElementById("popup").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "none";
-});
 
-document.getElementById("home-button").addEventListener("click", function() {
-    document.getElementById("popup").style.display = "none";
+
+
+homeButton.addEventListener("click", function() {
+    popup.style.display = "none";
 });
